1ac0710f664f8ae582f0e6009ba4600ea4d6d127
Hornet <fa18superhornet@protonmail.com>
2022-06-16 22:46:09 +0600

    first steps(header)

---
+++ src/Tltle/Title.js
@@ -0,0 +1,5 @@
+import {TitleStyles} from "./Title.styles";
+
+export const Title = () => {
+    return <TitleStyles>To-do-shka</TitleStyles>
+};
---
+++ src/Tltle/Title.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const TitleStyles = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  font-size: 3rem;
+  text-align: center;
+  color: #57b770;
+  background-color: #1e2226;
+`;
